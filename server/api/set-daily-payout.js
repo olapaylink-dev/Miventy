cb12dbef7a93f63dafdf4c087abff7fdb1fe9969
admis <admisrael2012@gmail.com>
2025-12-15 05:55:29 +0100

    Daily payout set

--- server/api/set-daily-payout.js
+++ server/api/set-daily-payout.js
@@ -2,43 +2,44 @@ const sharetribeIntegrationSdk = require('sharetribe-flex-integration-sdk');
 
 module.exports = async (req, res)  =>  {
 
-  const data = req.body;
-  const id = data.sId; //stripeId
+  const id = req.body;
  
     const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-    // const balanceSettingsResponse = await stripe.balanceSettings.retrieve({
-    //   stripeAccount: '{{CONNECTED_ACCOUNT_ID}}',
-    // });
+    const balanceSettingsResponse = await stripe.balanceSettings.retrieve({
+      stripeAccount: id,
+    });
 
-    console.log("Setting daily payout")
-
-    const balanceSettings = await stripe.balanceSettings.update(
-      {
-        payments: {
-          payouts: {
-            schedule: {
-              interval: 'daily'
+    const statuss = balanceSettingsResponse?.payments?.payouts?.status;
+    if(statuss !== "enabled"){
+        console.log("statuss",statuss)
+        const balanceSettings = await stripe.balanceSettings.update(
+          {
+            payments: {
+              payouts: {
+                schedule: {
+                  interval: 'daily'
+                },
+              },
             },
           },
-        },
-      },
-      {
-        stripeAccount: id
-      }
-    );
+          {
+            stripeAccount: id
+          }
+        );
 
-    const { status } = balanceSettings;
-    console.log(balanceSettings);
-    res
-    .status(200)
-    .set('Content-Type', 'application/transit+json')
-    .send(
-      {
-        balanceSettings
-      }
-    )
-    .end();
+        const { status } = balanceSettings;
+        res
+        .status(200)
+        .set('Content-Type', 'application/transit+json')
+        .send(
+          {
+            balanceSettings
+          }
+        )
+        .end();
+    }
+   
 
     
 }
