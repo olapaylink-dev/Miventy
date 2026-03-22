2db3d9017a2e22d516d4a14dc85b21bb5daad916
admis <admisrael2012@gmail.com>
2026-03-22 11:45:46 +0100

    Hide and show long and short cart info

--- src/containers/ListingPage/ListingPageCarousel.js
+++ src/containers/ListingPage/ListingPageCarousel.js
@@ -245,6 +245,7 @@ export const ListingPageComponent = props => {
   const [about,setAbout] = useState("");
   const favourites = currentUser?.attributes?.profile?.protectedData?.favourites || [];
   const favouriteUsers = currentUser?.attributes?.profile?.protectedData?.favouriteUsers || [];
+  const [showFull, setShowFull] = useState(false);
 
   ////console.log(reviews,"   nnnnnnnnnnnnnnnncccccccccccccccccccc")
 
@@ -1615,10 +1616,17 @@ console.log(userLocation,"   vvvvvvvvvvvvvvvvc77777777777777cccccccccccccc")
           </div>
         :""}
 
-        <div className={css.cart_modal}>
+        {showFull?
+          <div className={css.cart_modal}>
            <div>
                   <div className={css.aside}>
-                    <div className={css.flex_row_3}><span>{intl.formatMessage({ id: 'Dashboard.startingFrom' })}</span><span className={css.amount}>€{originalPrice !== undefined?(originalPrice.amount/100).toFixed(2):(price.amount/100).toFixed(2)}</span></div>
+                    <div className={css.flex_row_9}>
+                      <div className={css.flex_row_3}><span>{intl.formatMessage({ id: 'Dashboard.startingFrom' })}</span><span className={css.amount}>€{originalPrice !== undefined?(originalPrice.amount/100).toFixed(2):(price.amount/100).toFixed(2)}</span></div>
+                      <svg onClick={e=>setShowFull(false)} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+                        <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
+                      </svg>
+                    </div>
+                       
                     <p>Items in cart</p>
                     {currentUser?
                       <CartItems currentUser={currentUser} 
@@ -1663,6 +1671,16 @@ console.log(userLocation,"   vvvvvvvvvvvvvvvvc77777777777777cccccccccccccc")
                   
                 </div>
         </div>
+        :
+        <div className={classNames(css.cart_modal,css.flex_row_9)}>
+          <div className={css.flex_row_3}><span>{intl.formatMessage({ id: 'Dashboard.startingFrom' })}</span><span className={css.amount}>€{originalPrice !== undefined?(originalPrice.amount/100).toFixed(2):(price.amount/100).toFixed(2)}</span></div>
+          <svg onClick={e=>setShowFull(true)} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-caret-up" viewBox="0 0 16 16">
+            <path d="M3.204 11h9.592L8 5.519zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659"/>
+          </svg>
+        </div>
+        }
+        
+       
 
       </LayoutSingleColumn>
     </Page>
